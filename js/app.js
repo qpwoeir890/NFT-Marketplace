b4124e3250a30b847ab40753c55ef9bfb6674c9e
qpwoeir890 <x2702813@gmail.comgit config --global user.email x2702813@gmail.com>
2024-06-10 11:18:12 +0500

    finish- section-6, section-7, section-8, footer, Create account

--- js/app.js
+++ js/app.js
@@ -13,3 +13,36 @@ window.addEventListener("scroll", () => {
 toggleBtn.addEventListener("click",()=>{
   headerList.classList.toggle("header__list-show");
 })
+
+// setInterval(() => {
+//   const time = document.querySelector(".display #time");
+//   let date = new Date();
+//   let hours = date.getHours();
+//   let minutes = date.getMinutes();
+//   let seconds = date.getSeconds();
+//   let day_night = "AM";
+//   if (hours > 12) {
+//     day_night = "PM";
+//     hours = hours - 12;
+//   }
+//   if (seconds < 10) {
+//     seconds = "0" + seconds;
+//   }
+//   if (minutes < 10) {
+//     minutes = "0" + minutes;
+//   }
+//   if (hours < 10) {
+//     hours = "0" + hours;
+//   }
+//   time.textContent = hours + ":" + minutes + ":" + seconds 
+// })
+
+let hrs = document.getElementById("hrs");
+let minut = document.getElementById("minut");
+let second = document.getElementById("second");
+setInterval(()=>{
+  let currenTime = new Date();
+hrs.innerHTML = currenTime.getHours();
+minut.innerHTML = currenTime.getMinutes();
+second.innerHTML = currenTime.getSeconds();
+},1000)
