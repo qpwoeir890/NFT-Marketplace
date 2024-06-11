8eb9bf305fbe9721c690550a55e91ec18187abd2
qpwoeir890 <x2702813@gmail.comgit config --global user.email x2702813@gmail.com>
2024-06-11 08:54:39 +0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,41 +1,6 @@
-https://bright-gumdrop-4c2b38.netlify.applet header = document.querySelector(".header");
-let toggleBtn = document.querySelector(".header__btn-homburger");
-let headerList = document.querySelector(".header__list");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 10) {
-    header.classList.add("header__shrink");
-  } else {
-    header.classList.remove("header__shrink");
-  }
-});
 
-toggleBtn.addEventListener("click",()=>{
-  headerList.classList.toggle("header__list-show");
-})
 
-// setInterval(() => {
-//   const time = document.querySelector(".display #time");
-//   let date = new Date();
-//   let hours = date.getHours();
-//   let minutes = date.getMinutes();
-//   let seconds = date.getSeconds();
-//   let day_night = "AM";
-//   if (hours > 12) {
-//     day_night = "PM";
-//     hours = hours - 12;
-//   }
-//   if (seconds < 10) {
-//     seconds = "0" + seconds;
-//   }
-//   if (minutes < 10) {
-//     minutes = "0" + minutes;
-//   }
-//   if (hours < 10) {
-//     hours = "0" + hours;
-//   }
-//   time.textContent = hours + ":" + minutes + ":" + seconds 
-// })
 
 let hrs = document.getElementById("hrs");
 let minut = document.getElementById("minut");
